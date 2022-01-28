ef66b641167ef73292fc06775cbff0901f52cf83
Edder Cid <yeyo.foster@gmail.com>
2022-01-28 03:08:27 -0600

    Se agregan activities de SignUp y ForgotPassword

--- app/src/main/java/eddercid/com/kotlinfirebaseapp/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/eddercid/com/kotlinfirebaseapp/activities/ForgotPasswordActivity.kt
@@ -1,9 +1,10 @@
-package eddercid.com.kotlinfirebaseapp
+package eddercid.com.kotlinfirebaseapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import eddercid.com.kotlinfirebaseapp.R
 
-class MainActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
---
+++ app/src/main/java/eddercid/com/kotlinfirebaseapp/activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package eddercid.com.kotlinfirebaseapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eddercid.com.kotlinfirebaseapp.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
