7515da092e929a7d1d9aee076ea7f7a75e2459dd
Edder Cid <yeyo.foster@gmail.com>
2022-01-28 00:31:46 -0600

    Cambio de package en activities

--- app/src/main/java/eddercid/com/kotlinfirebaseapp/activities/LoginActivity.kt
+++ app/src/main/java/eddercid/com/kotlinfirebaseapp/activities/LoginActivity.kt
@@ -1,7 +1,8 @@
-package eddercid.com.kotlinfirebaseapp
+package eddercid.com.kotlinfirebaseapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import eddercid.com.kotlinfirebaseapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
