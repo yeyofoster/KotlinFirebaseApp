77fe5f5f971c20d604baa0cf91398454f02ca05d
Edder Cid <yeyo.foster@gmail.com>
2022-01-28 05:31:48 -0600

    Funcionalidad temporal a LoginActivity

--- app/src/main/java/eddercid/com/kotlinfirebaseapp/activities/LoginActivity.kt
+++ app/src/main/java/eddercid/com/kotlinfirebaseapp/activities/LoginActivity.kt
@@ -1,12 +1,45 @@
 package eddercid.com.kotlinfirebaseapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import eddercid.com.kotlinfirebaseapp.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import eddercid.com.kotlinfirebaseapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.buttonCreateAccount.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonSignIn.setOnClickListener {
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            signInWithEmailAndPassword(email, password)
+        }
+
+        binding.buttonLoginWithGoogle.setOnClickListener {
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            singInWithGoogle(email, password)
+        }
+
+        setContentView(binding.root)
+    }
+
+    private fun singInWithGoogle(email: String, password: String) {
+        Log.i("Values", "Email: $email Password: $password")
+    }
+
+    private fun signInWithEmailAndPassword(email: String, password: String) {
+        Log.i("Values", "Email: $email Password: $password")
     }
 }
